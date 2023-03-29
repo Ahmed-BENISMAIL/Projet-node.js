4b1eb79aafadfc28053f6d3be397d98dcb29b0eb
Your Name <you@example.com>
2023-03-29 16:02:41 +0100

    backend

---
+++ models/article.js
@@ -0,0 +1,56 @@
+const Sequelize = require('sequelize');
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('article', {
+    id: {
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true
+    },
+    libelle: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    prix: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    qtestock: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    imagearticle: {
+      type: DataTypes.STRING(300),
+      allowNull: false
+    },
+    scategorieid: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'scategorie',
+        key: 'id'
+      }
+    }
+  }, {
+    sequelize,
+    tableName: 'article',
+    timestamps: false,
+    indexes: [
+      {
+        name: "PRIMARY",
+        unique: true,
+        using: "BTREE",
+        fields: [
+          { name: "id" },
+        ]
+      },
+      {
+        name: "scategorieid",
+        using: "BTREE",
+        fields: [
+          { name: "scategorieid" },
+        ]
+      },
+    ]
+  });
+};
---
+++ models/init-models.js
@@ -0,0 +1,24 @@
+var DataTypes = require("sequelize").DataTypes;
+var _article = require("./article");
+var _categorie = require("./categorie");
+var _scategorie = require("./scategorie");
+
+function initModels(sequelize) {
+  var article = _article(sequelize, DataTypes);
+  var categorie = _categorie(sequelize, DataTypes);
+  var scategorie = _scategorie(sequelize, DataTypes);
+
+  scategorie.belongsTo(categorie, { as: "categorie", foreignKey: "categorie_id"});
+  categorie.hasMany(scategorie, { as: "scategories", foreignKey: "categorie_id"});
+  article.belongsTo(scategorie, { as: "scategorie", foreignKey: "scategorieid"});
+  scategorie.hasMany(article, { as: "articles", foreignKey: "scategorieid"});
+
+  return {
+    article,
+    categorie,
+    scategorie,
+  };
+}
+module.exports = initModels;
+module.exports.initModels = initModels;
+module.exports.default = initModels;
